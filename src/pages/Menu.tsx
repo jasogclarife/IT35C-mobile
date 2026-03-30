15c821e47fa6ba7f4bd0cfc63193ab0f4e33e0b7
jasogclarife <jasogclarife2004@gmail.com>
2026-03-30 22:50:21 +0800

    button

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,28 +1,17 @@
-import { IonButton, IonContent, IonHeader, IonPage, IonTitle,IonToolbar, useIonRouter } from "@ionic/react";
+import { IonHeader, IonPage, IonTitle,IonToolbar } from "@ionic/react";
 
-const menu: React.FC =() =>{
-  const navigation = useIonRouter();
-
-  const doLogin = () => {
-    navigation.push('/app', 'forward', 'replace');
-  }
+const Menu: React.FC =() =>{
 
     return(
       <IonPage>
         <IonHeader>
          <IonToolbar>
-            <IonTitle> menu</IonTitle>
+            <IonTitle> Menu</IonTitle>
          </IonToolbar>
         </IonHeader>
-        <IonContent className="ion-padding">
-
-            <IonButton expand="full" onClick={()=> doLogin()}>
-                Login
-            </IonButton>
-        </IonContent>
       </IonPage>
 
     );
 };
 
-export default menu;
+export default Menu;
